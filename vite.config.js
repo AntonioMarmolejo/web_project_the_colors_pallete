33ef7508b6f0fa15cd3af96b00782fbb9ce658e2
Antonio Marmolejo <djcamg71@gmail.com>
2025-07-03 19:54:32 -0500

    feat: cración del archivo para desplegar el proyecto en github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://antoniomarmolejo.github.io/web_project_the_colors_pallete"
 })
