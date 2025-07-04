0a50a27390fc4d4bb04a2310f5b617e98151d3e1
Antonio Marmolejo <djcamg71@gmail.com>
2025-07-04 01:13:57 -0500

    feat: creacion de las paletas de colores

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,266 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/Vitelogo.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+    return (
+        <>
+            <header className="header">
+                <h1 className="header__title">PALETA DE<br /><span className="span">COLORES</span><br />PARA TU PROFESIÓN </h1>
+            </header>
+            <main className="page">
+                <div className="musica">
+                    <h2 className="title">Música</h2>
+                    <div className="page__items">
+                        <div className="item musica__1">
+                            <div className="item__title">#E39AEB</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(22,84,19,4)</p>
+                        </div>
+                        <div className="item musica__2">
+                            <div className="item__title">#BA2771</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(95,70,29,15)</p>
+                        </div>
+                        <div className="item musica__3">
+                            <div className="item__title">#361C5D</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(79,82,25,12)</p>
+                        </div>
+                        <div className="item musica__4">
+                            <div className="item__title">#B54FD3</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(27,67,0,0)</p>
+                        </div>
+                        <div className="item musica__5">
+                            <div className="item__title">#080409</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(24,28,21,92)</p>
+                        </div>
+                    </div>
+                </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Paleta de Colores Hexagonales</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+                <div className="psicologia">
+                    <h2 className="title">Psicología</h2>
+                    <div className="page__items">
+                        <div className="item psicologia__1">
+                            <div className="item__title">#085C5D</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(93,30,48,16)</p>
+                        </div>
+                        <div className="item psicologia__2">
+                            <div className="item__title">#09979F</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(92,7,25,1)</p>
+                        </div>
+                        <div className="item psicologia__3">
+                            <div className="item__title">#293A4D</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(57,4,8,0)</p>
+                        </div>
+                        <div className="item psicologia__4">
+                            <div className="item__title">#8474A1</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(48,44,12,2)</p>
+                        </div>
+                        <div className="item psicologia__5">
+                            <div className="item__title">#CCA9DA</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(19,29,0,0)</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="cafeteria">
+                    <h2 className="title">Cafetería</h2>
+                    <div className="page__items">
+                        <div className="item cafeteria__1">
+                            <div className="item__title">#9D9CA2</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(35,26,19,5)</p>
+                        </div>
+                        <div className="item cafeteria__2">
+                            <div className="item__title">#1B383E</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(86,44,48,38)</p>
+                        </div>
+                        <div className="item cafeteria__3">
+                            <div className="item__title">#A46935</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(26,47,71,13)</p>
+                        </div>
+                        <div className="item cafeteria__4">
+                            <div className="item__title">#5A483D</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(47,47,53,33)</p>
+                        </div>
+                        <div className="item cafeteria__5">
+                            <div className="item__title">#6C7E5E</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(52,28,51,12)</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="carpinteria">
+                    <h2 className="title">Carpintería</h2>
+                    <div className="page__items">
+                        <div className="item carpinteria__1">
+                            <div className="item__title">#967546</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(31,38,61,15)</p>
+                        </div>
+                        <div className="item carpinteria__2">
+                            <div className="item__title">#4F311D</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(44,57,74,45)</p>
+                        </div>
+                        <div className="item carpinteria__3">
+                            <div className="item__title">#797067</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(43,36,38,18)</p>
+                        </div>
+                        <div className="item carpinteria__4">
+                            <div className="item__title">#A89C90</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(29,26,28,7)</p>
+                        </div>
+                        <div className="item carpinteria__5">
+                            <div className="item__title">#DFC69F</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(11,17,29,2)</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="inmoviliaria">
+                    <h2 className="title">Inmoviliaria</h2>
+                    <div className="page__items">
+                        <div className="item inmoviliaria__1">
+                            <div className="item__title">#174966</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(90,44,30,19)</p>
+                        </div>
+                        <div className="item inmoviliaria__2">
+                            <div className="item__title">#38627A</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(77,37,26,12)</p>
+                        </div>
+                        <div className="item inmoviliaria__3">
+                            <div className="item__title">#BC8D6D</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(21,35,44,7)</p>
+                        </div>
+                        <div className="item inmoviliaria__4">
+                            <div className="item__title">#A9725C</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(25,44,47,11)</p>
+                        </div>
+                        <div className="item inmoviliaria__5">
+                            <div className="item__title">#E8E6DE</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(9,7,8,0)</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="mecanica">
+                    <h2 className="title">Mecánica</h2>
+                    <div className="page__items">
+                        <div className="item mecanica__1">
+                            <div className="item__title">#333437</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(64,49,47,45)</p>
+                        </div>
+                        <div className="item mecanica__2">
+                            <div className="item__title">#C66717</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(18,54,90,5)</p>
+                        </div>
+                        <div className="item mecanica__3">
+                            <div className="item__title">#19323B</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(86,47,47,41)</p>
+                        </div>
+                        <div className="item mecanica__4">
+                            <div className="item__title">#3F676B</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(72,32,37,16)</p>
+                        </div>
+                        <div className="item mecanica__5">
+                            <div className="item__title">#78837F</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(47,29,31,11)</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="joyeria">
+                    <h2 className="title">Joyería</h2>
+                    <div className="page__items">
+                        <div className="item joyeria__1">
+                            <div className="item__title">#BD9127</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(20,33,84,7)</p>
+                        </div>
+                        <div className="item joyeria__2">
+                            <div className="item__title">#E3C57D</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(9,19,45,1)</p>
+                        </div>
+                        <div className="item joyeria__3">
+                            <div className="item__title">#984C64</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(29,61,32,15)</p>
+                        </div>
+                        <div className="item joyeria__4">
+                            <div className="item__title">#571B33</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(40,79,41,43)</p>
+                        </div>
+                        <div className="item joyeria__5">
+                            <div className="item__title">#210E18</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(53,76,46,71)</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="turismo">
+                    <h2 className="title">Turismo</h2>
+                    <div className="page__items">
+                        <div className="item turismo__1">
+                            <div className="item__title">#DEC7B5</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(12,17,20,1)</p>
+                        </div>
+                        <div className="item turismo__2">
+                            <div className="item__title">#F0E7DC</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(6,7,9,0)</p>
+                        </div>
+                        <div className="item turismo__3">
+                            <div className="item__title">#ADCECF</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(32,8,10,1)</p>
+                        </div>
+                        <div className="item turismo__4">
+                            <div className="item__title">#346157</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(77,32,50,18)</p>
+                        </div>
+                        <div className="item turismo__5">
+                            <div className="item__title">#232123</div>
+                            <div className="item__subtitle">CMYK</div>
+                            <p className="number">(66,55,52,60)</p>
+                        </div>
+                    </div>
+
+                </div>
+                <footer className="footer">
+                    <h3 className="footer__title">Antonio Marmolejo - Web Developer</h3>
+                </footer>
+            </main>
+        </>
+    );
 }
 
 export default App;
